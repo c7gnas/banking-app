810745c708b725e4a71c3714992e719e1e216110
Aniruddha <azizul.muktadir.chowdhury@g.bracu.ac.bd>
2024-09-10 06:24:42 +0600

    headerBox and totalBalanceBox Ui componenets added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,28 @@
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import React from 'react'
+
+const Home = () => {
+  const loggedIn = { firstName: "Aniruddha" };
+  return (
+   <section className='home'>
+    <div className="home-content"></div>
+    <header className='home-header'>
+      <HeaderBox
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstName || 'Guest'}
+        subtext="Access and manage your bank transactions efficiently"
+      />
+
+      <TotalBalanceBox
+        accounts = {[]}
+        totalBanks = {1}
+        totalCurrentBalance={1250.35}
+      /> 
+    </header>
+   </section>
+  )
+}
+
+export default Home
