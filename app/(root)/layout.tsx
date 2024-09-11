fc531cebb35b99faba1e72957fe5e9d918661570
Aniruddha <azizul.muktadir.chowdhury@g.bracu.ac.bd>
2024-09-12 04:48:49 +0600

    added mobile navigation bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,26 @@
 import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Aniruddha', lastName: 'Chowdhury'}
-  
+  const loggedIn = { firstName: "Aniruddha", lastName: "Chowdhury" };
+
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
-        {children}    
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+      </div>
+      {children}
     </main>
   );
 }
