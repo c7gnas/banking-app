8aaf24565f0cb8471285a8226231d9d43632448c
Aniruddha <azizul.muktadir.chowdhury@g.bracu.ac.bd>
2024-09-12 05:31:07 +0600

    Improved RightSidebar, added bank styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,36 @@
-import HeaderBox from '@/components/ui/HeaderBox'
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
-import React from 'react'
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Aniruddha" };
+  const loggedIn = {
+    firstName: "Aniruddha",
+    lastName: "Chowdhury",
+    email: "aniruddha231@gmail.com",
+  };
   return (
-   <section className='home'>
-    <div className="home-content"></div>
-    <header className='home-header'>
-      <HeaderBox
-        type="greeting"
-        title="Welcome"
-        user={loggedIn?.firstName || 'Guest'}
-        subtext="Access and manage your bank transactions efficiently"
-      />
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your bank transactions efficiently"
+          />
 
-      <TotalBalanceBox
-        accounts = {[]}
-        totalBanks = {3}
-        totalCurrentBalance={1250.35}
-      /> 
-    </header>
-   </section>
-  )
-}
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={3}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
